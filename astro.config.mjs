8e7eb082fa9ce671165ad03bc5369d43f5ea3401
Chris <cservius@proton.me>
2022-10-02 01:14:32 -0300

    updates and corrections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    compress({
+      html: { collapseWhitespace: true, removeComments: true },
+    }),
+  ],
   site: 'https://www.cservius.com',
 });
